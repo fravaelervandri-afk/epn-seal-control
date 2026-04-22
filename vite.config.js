7e746f12eb4fa7456dc7f23bf0a1b4c6d44d2a43
fravaelervandri-afk <fravaelervandri@gmail.com>
2026-04-22 17:03:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
         input: resolve(__dirname, 'input.html'),
         scan: resolve(__dirname, 'scan.html'),
         pelaporan: resolve(__dirname, 'pelaporan.html'),
-        'daftar-pelaporan': resolve(__dirname, 'daftar-pelaporan.html'),
-        'daftar-data': resolve(__dirname, 'daftar-data.html'),
-        'history': resolve(__dirname, 'history.html')
+        daftarData: resolve(__dirname, 'daftar-data.html'),
+        history: resolve(__dirname, 'history.html')
+        daftarpelaporan: resolve(__dirname, 'daftar-pelaporan.html'),
       }
     }
   }
