a123a858005cf1a9f01f128d6dcb00193885ef0d
fravaelervandri-afk <fravaelervandri@gmail.com>
2026-04-27 15:56:25 +0700

    Add files via upload

---
+++ src/pages/Verify/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Verify from './Verify.jsx';
+import '../../index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Verify />
+  </React.StrictMode>
+);
