c6397607a548535ecdcf139a45ca2319cd39de3f
fravaelervandri-afk <fravaelervandri@gmail.com>
2026-04-27 16:39:44 +0700

    Refactor import statements in main.jsx

--- src/pages/Verify/main.jsx
+++ src/pages/Verify/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Verify from './Verify.jsx';
 import '../../index.css';
-import { supabase } from '../../config/supabase';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
