048a1b940f3a19e1646a668d127f319e4ebe35e2
fravaelervandri-afk <fravaelervandri@gmail.com>
2026-04-27 16:34:07 +0700

    Update main.jsx

--- src/pages/Verify/main.jsx
+++ src/pages/Verify/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Verify from './Verify.jsx';
 import '../../index.css';
+import { supabase } from '../../config/supabase';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
